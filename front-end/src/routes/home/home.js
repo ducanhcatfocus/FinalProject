499192959d333fc2df4a0857139a6316a5401fac
catstare <66560895+catstare@users.noreply.github.com>
2022-06-28 20:35:42 +0700

    liftbar

--- front-end/src/routes/home/home.js
+++ front-end/src/routes/home/home.js
@@ -1,56 +1,53 @@
-import React from 'react';
+import React from "react";
 import "./home.scss";
-import NavbarContainer from '../../components/navbar/navbar'
-import SearchbarComponent from '../../components/searchbar/searchbar';
-import MainContent from '../../components/maincontent/maincontent';
-
+import NavbarContainer from "../../components/navbar/navbar";
+import SearchbarComponent from "../../components/searchbar/searchbar";
+import MainContent from "../../components/maincontent/maincontent";
 
 const Home = () => {
   return (
-    
     <div className="home__container">
-      
-        <SearchbarComponent/>
-      
-        <div>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-          <h2>Dit me thang duc anh</h2>
-        </div>
-        {/* 
+      <SearchbarComponent />
+
+      <div>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+        <h2>Leftbar</h2>
+      </div>
+      {/* 
         <div className="home__body">
           <NavbarContainer/>
           <MainContent/>
         </div> */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
